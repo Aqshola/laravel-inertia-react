817c413a8e6ef9f14e1471beee44539c75b34202
PNMGROUP\aafid0127 <aafid0127@pnm.co.id>
2024-03-12 10:58:09 +0700

    feat: init laravel inertia antd

---
+++ resources/js/pages/welcome.jsx
@@ -0,0 +1,25 @@
+import { Head } from "@inertiajs/react";
+import { Flex, Typography } from "antd";
+import React from "react";
+
+export default function Welcome() {
+    const STYLING={
+        flexContainerStyle:{
+            width:"100%",
+            minHeight:'100vh',
+        },
+        headingStyle:{
+            textAlign:'center'
+        }
+    }
+    return (
+        <>
+        <Head>
+            <title>Welcome Page</title>
+        </Head>
+        <Flex style={STYLING.flexContainerStyle} justify="center" align="center" vertical>
+            <Typography.Title level={5} style={STYLING.headingStyle}>Welcome to Larvel Inertia React</Typography.Title>
+        </Flex>
+        </>
+    );
+}
